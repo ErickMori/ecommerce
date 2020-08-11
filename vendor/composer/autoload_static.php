202840d81090e2bff1b5fccac4703ab5398a20c9
BuildTools <unconfigured@null.spigotmc.org>
2020-08-10 22:18:59 -0300

    autoload hcode

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit9b49c9d7ab57388a5f81326ecb9fd3ac
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit9b49c9d7ab57388a5f81326ecb9fd3ac
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit9b49c9d7ab57388a5f81326ecb9fd3ac::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit9b49c9d7ab57388a5f81326ecb9fd3ac::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit9b49c9d7ab57388a5f81326ecb9fd3ac::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit9b49c9d7ab57388a5f81326ecb9fd3ac::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit9b49c9d7ab57388a5f81326ecb9fd3ac::$classMap;
 
